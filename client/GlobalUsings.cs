d93f9f44608fc2c2dc48f7d4c60bc1a3ac6b7e0a
Manuel Guzman <manuguzman8@gmail.com>
2025-09-20 00:18:17 -0600

    fix: Updating usings and files

--- client/GlobalUsings.cs
+++ client/GlobalUsings.cs
@@ -1,6 +1,5 @@
-global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using Microsoft.AspNetCore.Components;
 global using Microsoft.AspNetCore.Components.Web;
 global using System.Net.Http.Json;
-global using client.Services;
 global using client.Models;
-global using MudBlazor.Services;
+global using client.Services;
--- client/Program.cs
+++ client/Program.cs
@@ -1,20 +1,23 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using client;
+using client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+// Configure root components
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Add MudBlazor services
-builder.Services.AddMudServices();
-
-// Configure HttpClient for API communication
+// Configure HTTP client
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri("http://localhost:5184/") // API base URL
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
 
-// Add application services
-builder.Services.AddScoped<AuthService>();
+// Register application services
 builder.Services.AddScoped<ProductService>();
+builder.Services.AddScoped<AuthService>();
 
+// Build and run the application
 await builder.Build().RunAsync();
